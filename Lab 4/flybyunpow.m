2c24aa24f4a7c086c788da834e2ce8ebe27ae95e
Davide Iafrate <davide.iafrate@mail.polimi.it>
2020-12-04 19:08:29 +0100

    Completed function for unpowered flyby of a planet

--- Lab 4/flybyunpow.m
+++ Lab 4/flybyunpow.m
@@ -1,20 +1,23 @@
-function [rp,a,e,delta,vinfP] = flybyunpow(vinfM,mu,Delta,u)
-%FLYBYUNPOW function that computes an unpowered flyby manoeuvre
+function [rp,a,e,delta] = flybyunpow(vinf,mu,Delta,u)
+%FLYBYUNPOW function that computes an unpowered flyby manoeuvre in the
+%           planetocentric frame of reference
+%
+% PROTOTYPE:
+%   [rp,a,e,delta] = flybyunpow(vinf,mu,Delta,u)
+% 
 % INPUT:
-%   vinfM[3x1]  incoming velocity at infinity in the planetocentric 
+%   vinf[3x1]   incoming velocity at infinity in the planetocentric 
 %               reference frame    [km/s]
 %   mu[1]       gravitational parameter of the planet           [km^3/s^2]
-%   Delta[3x1]  impact parameter vector                         [km]
+%   Delta[1]  impact parameter vector                         [km]
 %   u[3x1]      unit axis of rotation vector around which the 
 %               velocity is rotated     [-]
-
+% 
 % OUTPUT:
 %   rp[3x1]     perigee radius wrt planet                       [km]
 %   a[1]        semi-major axis of the hyperbola                [km]
 %   e[1]        eccentricity of the hyperbola                   [-]
 %   delta[1]    turning angle of hyperbola                      [rad]
-%   vinfP[3x1]  outgoing velocity at infinity in the planetocentric
-%               reference frame                                 [km/s]
 % 
 % AUTHORS:
 %   Davide Iafrate
@@ -22,7 +25,10 @@ function [rp,a,e,delta,vinfP] = flybyunpow(vinfM,mu,Delta,u)
 % REVISIONS:
 %   04-12-2020  first revision
 
-vinf = norm(vinfM);
+if nargin < 4
+    u = 0;
+end
+vinf = norm(vinf);
 
 % Calculate semi-major axis
 a = -mu/vinf^2;
@@ -36,9 +42,6 @@ e = 1/sin(delta/2);
 % Calculate magnitude of perigee radius
 rp = a*(1-e);
 
-% Rotation about the u axis using Rodrigues' formula
-
-vinfP = vinfM*cos(delta) + sin(delta).*cross(u,v) + dot(u,vinfM).*u.*(1-cos(delta));
 
 
 end
